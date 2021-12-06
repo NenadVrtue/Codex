0f765de635220ccfd7d8dcbff281f3b72e8b3828
NenadVrtue <hello@vrtuedigital.com>
2021-12-06 09:20:44 +0100

    Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,10 @@ module.exports = {
 
       colors:{
         'dark-blue': '#323667',
-        'gray':'#9FB0BF',
-        'light-blue': '#29B8E8'
+        'gray':'#8C9CAC',
+        'light-blue': '#29B8E8',
+        'gradient-1': '#24E8B9',
+        'gradient-2': '#29A9F5'
       },
 
       maxWidth:{
