fc18edb50d8ecf76bf0058a02a3f1af3853935d8
NenadVrtue <hello@vrtuedigital.com>
2021-12-09 18:31:35 +0100

    SEO update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ module.exports = {
     title: `Codex Agencija`,
     description: `Codex Agencija Stevo Tadić s.p. pruža knjigovodstvene usluge, prilagođene poslovanju sa velikim, srednjim i malim preduzećima`,
     author: `VrtueDigital`,
-    defaultImage: 'OG.png'
+    titleTemplate: "%s | Codex Knjigovodstvena Agencija",
+    image: '/OG.png'
   },
   flags: {
     THE_FLAG: false
